4dc1715366e322f950f953948effc45073aeb701
Anusha Kirshna <anushakirshna@gmail.com>
2026-09-12 18:13:49 +0400

    Fix CORS

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="PriceHack API", description="Stateless Backend for PriceHac
 # Configure CORS for Next.js frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
